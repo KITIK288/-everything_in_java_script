20d4e5aff668511c1bfdec585f88f4fd0a2c01e8
macbook <svy1dan@gmail.com>
2024-12-01 18:48:14 +0300

    Программа на джава срипт, считающая количество слов в строке.

---
+++ work_with_string.js
@@ -0,0 +1,16 @@
+// программа, считающая количество слов строке
+
+string = "мяу мур мрр";
+const cleanedStr = string.replace(/[.,!?;]*/g, '');
+const words = cleanedStr.split(/\s+/);
+
+counter = 0
+
+
+for (i = 0; i < words.length; i++) {
+    console.log(words[i])
+    counter++
+}
+
+
+console.log("Количество слов -", counter)
