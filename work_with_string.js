8ef5f76316dbdc281549994afb599fe2540ae223
macbook <svy1dan@gmail.com>
2024-12-01 18:56:05 +0300

    Программа на джава срипт, считающая количество слов в строке.

--- work_with_string.js
+++ work_with_string.js
@@ -6,11 +6,9 @@ const words = cleanedStr.split(/\s+/);
 
 counter = 0
 
-
 for (i = 0; i < words.length; i++) {
     console.log(words[i])
     counter++
 }
-
 
 console.log("Количество слов -", counter)
